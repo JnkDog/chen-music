609cdaebd312b810d9f92695f8a3dfdd1fc2a760
JnKDog <530661401@qq.com>
2022-03-26 08:13:05 +0800

    Initial commit

---
+++ src/main/java/com/ruatech/chenmusic/ChenMusicApplication.java
@@ -0,0 +1,12 @@
+package com.ruatech.chenmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChenMusicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChenMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruatech/chenmusic/ChenMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruatech.chenmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChenMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
